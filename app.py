3405e82b9e629fe1b9de008e85f3154c1b6106e7
Rigvedsarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2024-03-07 15:46:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
-# app.py
-
 import os
-import whisper
 import streamlit as st
-import pandas as pd
-from converter import convert_audio   # Importing the convert_audio function
+from pydub import AudioSegment
 
-st.set_page_config(
-    page_title="Audio File Converter",
-    layout="wide",
-    initial_sidebar_state="auto",
-)
+st.title("MP3 Audio Converter")
 
-st.title("Audio File Converter")
+upload_path = "uploads/"
+download_path = "downloads/"
 
-uploaded_file = st.file_uploader("Upload audio file", type=["wav", "mp3", "ogg", "wma", "aac", "flac", "mp4", "flv"])
-output_format = st.selectbox("Select output format", ["mp3", "wav", "ogg", "wma", "aac", "flac"])
+def convert_audio(audio_file, output_format):
+    audio_data = AudioSegment.from_mp3(audio_file)
+    output_audio_file = audio_file.split('.')[0] + '.' + output_format
+    audio_data.export(os.path.join(download_path, output_audio_file), format=output_format)
+    return output_audio_file
+
+uploaded_file = st.file_uploader("Upload MP3 audio file", type=["mp3"])
 
 if uploaded_file is not None:
-    st.audio(uploaded_file, format='audio/' + uploaded_file.type.split('/')[-1])
-    
+    audio_bytes = uploaded_file.read()
+    with open(os.path.join(upload_path, uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.audio(audio_bytes, format='audio/mp3')
+
+    output_format = st.selectbox("Select output audio format", ["wav", "ogg", "wma", "aac", "flac", "flv"])
+
     if st.button("Convert"):
         with st.spinner("Converting..."):
-            output_file = convert_audio(uploaded_file.name, output_format)  # Pass the file name instead of the uploaded_file object
-        
-        st.success(f"Conversion complete. Download your converted file [here](output_file).")
+            output_audio_file = convert_audio(os.path.join(upload_path, uploaded_file.name), output_format)
+            converted_file_path = os.path.join(download_path, output_audio_file)
+            st.audio(open(converted_file_path, 'rb').read(), format='audio/' + output_format, label='Download Converted Audio')
+            st.success("Conversion complete. You can download the converted audio file.")
+
+else:
+    st.warning("Please upload an MP3 audio file.")
+
+st.markdown(
+    "<br><hr><center>Made by <a href='mailto:rigved.sarougi@henryharvin.com?subject=MP3 Audio Converter App'><strong>Rigved Sarougi</strong></a><hr>",
+    unsafe_allow_html=True)
